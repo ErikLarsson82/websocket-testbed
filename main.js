609742a0f1a233f6f9df387944002bfe5884f64f
Erik Larsson <erik.mattias.larsson@gmail.com>
2024-02-01 14:54:05 +0100

    Map with many vehicles

--- main.js
+++ main.js
@@ -1,7 +1,98 @@
 
-const OUTPUT_LOG = true
+import Map from 'ol/Map'
+import View from 'ol/View'
+import TileLayer from 'ol/layer/Tile'
+import VectorLayer from 'ol/layer/Vector'
+import VectorSource from 'ol/source/Vector'
+import XYZ from 'ol/source/XYZ'
+import Feature from 'ol/Feature'
+import Point from 'ol/geom/Point'
+import { Style, Fill, Stroke, Circle as CircleStyle } from 'ol/style'
+import { fromLonLat, toLonLat } from 'ol/proj'
+
+import lerp from 'lerp'
+
+const vehicleLayer = new VectorLayer({ source: new VectorSource() })
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new XYZ({
+        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      })
+    }),
+    vehicleLayer
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
+
+const OUTPUT_LOG = false
 const AMOUNT = 10000
-const RENDERING = 500
+const RENDERING = 1
+const OL_RENDERING = 1000
+const DELTA = 0.0072
+
+let previousPositions = []
+let currentPositions = []
+
+let timeSinceData = 0
+
+const fill = new Fill({ color: 'rgba(255,255,255,0.4)' })
+const stroke = new Stroke({ color: '#4a89f3', width: 1.25 })
+
+const features = new Array(OL_RENDERING).fill().map((_, idx) => {
+  const style = new Style({
+    image: new CircleStyle({
+      fill,
+      stroke,
+      radius: 23
+    }),
+    fill,
+    stroke,
+    renderer: (coordinates, state) => {
+      // console.log('time since', timeSinceData)
+      if (previousPositions.length === 0) return
+      const { context } = state
+      context.save()
+      // const previous = toLonLat([previousPositions[idx].long, previousPositions[idx].lat])
+      // debugger
+      // const lon = lerp(previous[0], coordinates[0], 0)
+      // const lat = lerp(previous[1], coordinates[1], 0)
+      context.translate(coordinates[0], coordinates[1])
+      context.beginPath()
+      context.arc(0,0,10,0,2*Math.PI)
+      context.fillStyle = '#0096FF'
+      context.fill()
+      context.restore()
+    }
+  })
+  const feature = new Feature({
+    geometry: new Point(fromLonLat([0, 0]))
+  })
+  feature.setStyle(style)
+  vehicleLayer.getSource().addFeature(feature)
+  return feature
+})
+
+function tick(ms, delta) {
+  timeSinceData += DELTA
+
+  if (previousPositions.length > 0 && currentPositions.length > 0) {
+    features.forEach((feature, idx) => {
+      const lon = lerp(previousPositions[idx].lon, currentPositions[idx].lon, timeSinceData)
+      const lat = lerp(previousPositions[idx].lat, currentPositions[idx].lat, timeSinceData)
+      // debugger
+      feature.getGeometry().setCoordinates(fromLonLat([lon, lat]))
+    })
+  }
+
+  requestAnimationFrame(tick)
+}
+requestAnimationFrame(tick)
 
 const sumStr = (acc, curr) => acc += curr
 
@@ -13,8 +104,6 @@ const ws = new WebSocket('ws://localhost:8080')
 ws.onopen = event => {
   OUTPUT_LOG && console.log('Connection open')
 }
-
-// let positions = []
 
 ws.onmessage = rawEvent => {
 
@@ -28,9 +117,16 @@ ws.onmessage = rawEvent => {
 
   if (event.type === 'data') {
     OUTPUT_LOG && console.log('Data received', event.data)
-    // positions = event.data
+    timeSinceData = 0
+    // previousPositions.length = 0
+    // currentPositions.length = 0
+    previousPositions = currentPositions
+    currentPositions = event.data.slice(0, OL_RENDERING)
     event.data.slice(0, RENDERING).forEach((pos, idx) => {
-      document.getElementById(`target-${idx}`).innerHTML = `Long: ${pos.long} - Lat: ${pos.lat}`
+      document.getElementById(`target-${idx}`).innerHTML = `Long: ${pos.lon} - Lat: ${pos.lat}`
+    })
+    event.data.slice(0, OL_RENDERING).forEach((pos, idx) => {
+      // features[idx].getGeometry().setCoordinates(fromLonLat([pos.lon, pos.lat]))
     })
     event.data.length = 0
   }
--- middleware.js
+++ middleware.js
@@ -1,3 +1,5 @@
+const lerp = require('lerp')
+
 const AMOUNT = 10000
 
 const WebSocket = require('ws')
@@ -7,13 +9,39 @@ const positions = new Array(AMOUNT).fill()
 
 const INTERVAL = 1000
 
+function lerpIt({ from, to, time }) {
+  return {
+    lon: lerp(from.lon, to.lon, time),
+    lat: lerp(from.lat, to.lat, time)
+  }
+}
+
 setInterval(() => {
   console.log('Updating values', new Date())
 
   positions.forEach((_, idx) => {
-    positions[idx] = {
-      lat: Math.random(),
-      long: Math.random()
+    if (positions[idx] === undefined) {
+      positions[idx] = {
+        from: {
+          lat: 59 + Math.random(),
+          lon: 17 + Math.random()
+        },
+        to: {
+          lat: 59 + Math.random(),
+          lon: 17 + Math.random()
+        },
+        time: 0
+      }
+    }
+    positions[idx].time += 0.01
+
+    if (positions[idx].time > 1) {
+      positions[idx].from = positions[idx].to
+      positions[idx].to = {
+        lat: 59 + Math.random(),
+        lon: 17 + Math.random()
+      }
+      positions[idx].time = 0
     }
   })
 }, INTERVAL)
@@ -37,7 +65,7 @@ wss.on('connection', ws => {
   pulseInterval = setInterval(() => {
     wss.clients.forEach(client => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({ type: 'data', data: positions }))
+        client.send(JSON.stringify({ type: 'data', data: positions.map(lerpIt) }))
       }
     })
   }, INTERVAL)
